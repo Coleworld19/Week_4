3107e649c9911b102f98075ea44beffbb493fc26
Cole Kelly <Cole.jkelly1@yahoo.com>
2021-05-04 01:15:42 -0400

    Monday day1

---
+++ Homework.sql
@@ -0,0 +1,44 @@
+--Question 1
+SELECT last_name
+From actor 
+Where last_name = 'Wahlberg';
+
+--Question 2
+SELECT amount
+From payment
+Where amount >= 3.99 and <= 5.99;
+
+--Question 3
+Select * 
+From inventory  
+
+
+--Question 4
+Select last_name
+From customer
+Where last_name = 'William'
+
+--Question 5
+select * 
+from staff
+
+--Question 6
+select
+
+--Question 7
+select
+
+--Question 8
+Select store_id, last_name
+From customer 
+Where last_name LIKE '%es' AND  store_id = 1;
+
+--Question 9
+Select
+From
+
+--Question 10
+Select rating, COUNT (rating)
+FROM film
+Group BY rating;
+
